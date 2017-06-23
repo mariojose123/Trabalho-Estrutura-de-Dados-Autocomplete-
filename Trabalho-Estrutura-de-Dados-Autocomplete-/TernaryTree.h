41fbad2a1fa54012e3dc69839d6427cdd76272f1
Mario Jose Kunz Filho <mariofilho4k@gmail.com>
2017-06-23 02:02:28 -0300

    Update TernaryTree.h

--- Trabalho-Estrutura-de-Dados-Autocomplete-/TernaryTree.h
+++ Trabalho-Estrutura-de-Dados-Autocomplete-/TernaryTree.h
@@ -2,10 +2,6 @@
 
 /*Estrutura da arvore ternaria*/
 struct Tnodo{
-=======
-/*Estrutura da arvore ternaria*/
-truct Tnodo{
->>>>>>> e62668d5187b6c096f61a1343abb6b77d1d80ddd
     char info;
     struct Tnodo *left;
     struct Tnodo *right;
@@ -16,7 +12,3 @@ truct Tnodo{
 typedef struct Tnodo pNodo;
 void Insert(char *word,pNodo **node,int pontos);
 pNodo* newNode(char word);
-<<<<<<< HEAD
-=======
-void  Sugestions(TipoPilha **sugestions,pNodo *node,char* str);
->>>>>>> e62668d5187b6c096f61a1343abb6b77d1d80ddd
